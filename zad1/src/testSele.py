b58c8605ebdcc5796b5a00211173fe5dbebfd55c
Smerski <merski70@gmail.com>
2022-01-19 14:52:32 +0100

    zad1

---
+++ zad1/src/testSele.py
@@ -0,0 +1,15 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import chromedriver_binary
+
+class MyTestCase(unittest.TestCase):
+    def test_title(self):
+        driver = webdriver.Chrome(executable_path='C:/Users/48531/OneDrive/Pulpit/Testowanie/laboratorium-15-Meruszka/zad1/driver/chromedriver.exe')
+        driver.get("https://duckduckgo.com/")
+        driver.find_element_by_id("search_form_input_homepage").send_keys("Selenium")
+        driver.find_element_by_id("search_button_homepage").submit()
+        self.assertEqual(driver.title, "DuckDuckGo — Prywatność — jeszcze prostsza.")
+        driver.quit()
+
