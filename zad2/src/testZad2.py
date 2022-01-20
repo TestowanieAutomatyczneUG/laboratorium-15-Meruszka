55efb05aef10173bbd5a3120109b114cd969aeda
Smerski <merski70@gmail.com>
2022-01-20 18:11:04 +0100

    zad2/czesc

--- zad2/src/testZad2.py
+++ zad2/src/testZad2.py
@@ -5,9 +5,9 @@ from selenium.webdriver.common.keys import Keys
 
 class MyTestCase(unittest.TestCase):
     def test_title(self):
-        driver = webdriver.Chrome(executable_path='C:/Users/48531/OneDrive/Pulpit/Testowanie/laboratorium-15-Meruszka/zad1/driver/chromedriver.exe')
-        driver.get("https://duckduckgo.com/")
-        driver.find_element_by_id("search_form_input_homepage").send_keys("Selenium")
+        driver = webdriver.Chrome(executable_path='C:/Users/48531/OneDrive/Pulpit/Testowanie/laboratorium-15-Meruszka/zad2/driver/chromedriver.exe')
+        driver.get("https://www.google.com/")
+        driver.find_element_by_class_name("gLFyf gsfi").send_keys('Kotki')
         driver.find_element_by_id("search_button_homepage").submit()
         self.assertEqual(driver.title, "Selenium at DuckDuckGo")
         driver.quit()
